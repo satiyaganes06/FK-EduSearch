99028ee25e0482bdc4565193f1ccca8013ad24cf
ainnajihah173 <ainnajihah173@gmail.com>
2023-05-25 03:21:31 +0800

    complaint

--- View/Complaint/userAddComplaint.php
+++ View/Complaint/userAddComplaint.php
@@ -20,8 +20,8 @@
     <!--CSS-->
     <link rel="stylesheet" href="../Common//css/navbar.css">
     <link rel="stylesheet" href="../Common//css/footer.css">
-    <link rel="stylesheet" href="css/expert_view_publication.css">
-
+    <link rel="stylesheet" href="css/user_addStyle.css">
+    
     <!-- Icon -->
     <link rel="shortcut icon" type="image/jpg" href="../../Asset/icon_logo.png" />
 
@@ -33,85 +33,59 @@
     include_once('../Common/html/userNavBar.html');
   ?>
 
-  <section class="flexSection">
-    <div class="mainSection d-flex flex-column mb-5 mt-3">
-
-        <div id="publicationDetails_Component">
-          
-          <div class="content_details">
-
-            <div class=" w-100">
-
-              <div class="d-flex justify-content-between  mb-3">
-                <h2 id="titlePage"><strong>Add Publication</strong></h2>
-                
-              </div>
-              
-              <table class="table align-middle">
-                <tbody>
-                  <tr>
-                    <th scope="row"><strong>Authors :</strong></th>
-                    <td>
-                        
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-                    
-                    </td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Publication : date </strong></th>
-                    <td><input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Journal :</strong></th>
-                    <td><input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" ></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Volume :</strong></th>
-                    <td><input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Issue :</strong></th>
-                    <td><input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Pages :</strong></th>
-                    <td><input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Publisher :</strong></th>
-                    <td><input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Description :</strong></th>
-                    <td id='pubDesc'><textarea  class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Scholar : article </strong></th>
-                    <td><input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-
-                  <tr>
-                    <th scope="row"><strong>Article : Source </strong></th>
-                    <td><input type="url" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"></td>
-                  </tr>
-                </tbody>
-              </table>
-              
-            </div>
-           
+  <section>
+    <div class="container text-center pt-3 pb-2"> 
+      <h4>Complaint Form</h4>
+      <p>Please fill out the following form with 
+        <br>your complaint. We will review your report
+        <br>and follow up as soon as possible</p>
+    </div>
+    <div class="container mt-3 p-3 align-items-center">
+      <div class="col-sm-7 mx-auto col-10 col-md-8 col-lg-6">
+        <form>
+          <div class="mb-2">
+            <label class="form-label" for="name">Name</label>
+            <input type="text" id="name" class="form-control" placeholder="James Cooper" disabled/>
           </div>
-          <button class="button_View btn-dark btn btn-block text-white mt-3"  data-mdb-ripple-color="dark"><strong>Review</strong></button>
+          <div class="mb-2">
+            <label class="form-label" for="email">Email</label>
+            <input type="email" id="email" class="form-control" placeholder="jamescooper@gmail.com" disabled />
+          </div>
 
-        </div> 
-        
+          <div class="row mb-2">
+            <div class="col">
+                <label class="form-label" for="date">Date</label>
+                <input type="date" id="date" class="form-control" />
+            </div>
+            <div class="col">
+                <label class="form-label" for="phoneNum">Phone Number</label>
+                <input type="tel" id="phoneNum" class="form-control" />
+            </div>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label" for="type">Type of Complaints</label>
+            <select class="form-select" aria-label="typeComplaint">
+              <option selected>Please Select...</option>
+              <option value="1">Unsatisfied Expert's Feedback</option>
+              <option value="2">Wrongly Assigned Research Area</option>
+            </select>
+          </div>
+
+          <!-- Message input -->
+          <div class="mb-3">
+            <label class="form-label" for="desc">Brief Description</label>
+            <textarea class="form-control" id="desc" rows="4"></textarea>
+          </div>
+          
+          <!-- button -->
+          <div class="button-box col-lg-12 mb-2">
+            <button class="btn btn-gray fw-bold">Cancel</button>
+            <button class="btn btn-primary ml-5 fw-bold">Submit</button>
+          </div>
+
+        </form>
+      </div>
     </div>
   </section>
 
