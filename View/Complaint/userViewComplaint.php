b60bc7c115a1d5287e713f83ee39758184ee24f4
ainnajihah173 <ainnajihah173@gmail.com>
2023-05-25 17:35:55 +0800

    --

--- View/Complaint/userViewComplaint.php
+++ View/Complaint/userViewComplaint.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FK-Edu Search</title>
@@ -24,8 +24,83 @@
     
     <!-- Icon -->
     <link rel="shortcut icon" type="image/jpg" href="../../Asset/icon_logo.png" />
+
+    <!-- Pie Chart -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+
 </head>
 <body>
-    
+  
+  <!-- Navbar -->
+  <?php
+    include_once('../Common/html/userNavBar.html');
+  ?>
+
+  <section>
+    <div class="container pt-3 pb-2"> 
+        <h6 class="fw-bold">Complaint Overview</h6>
+        <div class="row mt-3">
+            <div class="col-sm-3">
+                <div class="box" style="border-color:#FF0000;">
+                    <h5>In Investigation</h5>
+                    <p>3</p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="box" style="border-color:#FFD600;">
+                    <h5>On Hold</h5>
+                    <p>2</p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="box" style="border-color:#1FB712;">
+                    <h5>Resolved</h5>
+                    <p>1</p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <canvas id="myChart" style="width:100%;max-width:600px"></canvas>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-3 p-3 align-items-center">
+      
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <?php
+    include_once('../Common/html/footer.html');
+  ?>
+
+<script>
+    var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = [
+    "#b91d47",
+    "#00aba9",
+    "#2b5797",
+    "#e8c3b9",
+    "#1e7145"
+    ];
+
+    new Chart("myChart", {
+    type: "doughnut",
+    data: {
+        labels: xValues,
+        datasets: [{
+        backgroundColor: barColors,
+        data: yValues
+        }]
+    },
+    });
+</script>
+
+  <!-- MDB -->
+  <script type="text/javascript" src="../../Bootstrap/mdb.min.js"></script>
+  <!--Bootstrap 4 & 5 & jQuery Script-->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
