9d8cb64df08872cfe7792bf25223064c1cb9d8a4
nurnaz <nurulllnazwa@gmail.com>
2023-05-27 01:07:17 +0800

    Modul 2: General User
    Update coding

--- View/User/dashboard.php
+++ View/User/dashboard.php
@@ -34,10 +34,164 @@
   ?>
 
   <section>
-    <!---->
-    <div class="container mt-3 p-3 align-items-center">
-      tgrg
-    </div>
+    <table>
+      <tr>
+        <td>
+          <!--First Row Section-->
+          <div class="container pt-3 pb-2">
+                <div class="row mt-3">
+                    <div class="col-sm">
+                        <div class="box" style="text-align: center;">
+                            <h4><a href="">Software Engineering</a></h4> 
+                            <table class="center">
+                                <tr>
+                                  <th>100</th>
+                                  <th>98</th>
+                                </tr>
+                                <tr>
+                                  <td>Question</td>
+                                  <td>Solve</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="box" style="text-align: left;">
+                            <h5>Human Computer Interaction</h5><br>
+                            <h5>Web Engineering</h5><br>
+                            <h5>Software Maintenance & Evolution</h5>
+                        </div>
+                    </div>
+                    <div class="col-sm" >
+                        <div class="box" style="text-align: left;">
+                            <h5>Software Testing</h5><br>
+                            <h5>Formal Method</h5><br>
+                            <h5>Software Quality Assurance</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Second Row Section-->
+            <div class="container mt-3 p-3 align-items-center">
+                <div class="row mt-3">
+                    <div class="col-sm">
+                        <div class="box" style="text-align: center;">
+                            <h4><a href="">Software Engineering</a></h4> 
+                            <table class="center">
+                                <tr>
+                                  <th>100</th>
+                                  <th>98</th>
+                                </tr>
+                                <tr>
+                                  <td>Question</td>
+                                  <td>Solve</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="box" style="text-align: left;">
+                            <h5>Human Computer Interaction</h5><br>
+                            <h5>Web Engineering</h5><br>
+                            <h5>Software Maintenance & Evolution</h5>
+                        </div>
+                    </div>
+                    <div class="col-sm" >
+                        <div class="box" style="text-align: left;">
+                            <h5>Software Testing</h5><br>
+                            <h5>Formal Method</h5><br>
+                            <h5>Software Quality Assurance</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Third Row Section-->
+            <div class="container mt-3 p-3 align-items-center">
+                <div class="row mt-3">
+                    <div class="col-sm">
+                        <div class="box" style="text-align: center;">
+                            <h4><a href="">Software Engineering</a></h4> 
+                            <table class="center">
+                                <tr>
+                                  <th>100</th>
+                                  <th>98</th>
+                                </tr>
+                                <tr>
+                                  <td>Question</td>
+                                  <td>Solve</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="box" style="text-align: left;">
+                            <h5>Human Computer Interaction</h5><br>
+                            <h5>Web Engineering</h5><br>
+                            <h5>Software Maintenance & Evolution</h5>
+                        </div>
+                    </div>
+                    <div class="col-sm" >
+                        <div class="box" style="text-align: left;">
+                            <h5>Software Testing</h5><br>
+                            <h5>Formal Method</h5><br>
+                            <h5>Software Quality Assurance</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--Fourth Row Section-->
+            <div class="container mt-3 p-3 align-items-center">
+                <div class="row mt-3">
+                    <div class="col-sm">
+                        <div class="box" style="text-align: center;">
+                            <h4><a href="">Software Engineering</a></h4> 
+                            <table class="center">
+                                <tr>
+                                  <th>100</th>
+                                  <th>98</th>
+                                </tr>
+                                <tr>
+                                  <td>Question</td>
+                                  <td>Solve</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                    <div class="col-sm">
+                        <div class="box" style="text-align: left;">
+                            <h5>Human Computer Interaction</h5><br>
+                            <h5>Web Engineering</h5><br>
+                            <h5>Software Maintenance & Evolution</h5>
+                        </div>
+                    </div>
+                    <div class="col-sm" >
+                        <div class="box" style="text-align: left;">
+                            <h5>Software Testing</h5><br>
+                            <h5>Formal Method</h5><br>
+                            <h5>Software Quality Assurance</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </td>
+        <td>
+          <div class="imageDashboard"></div>
+        </td>
+      </tr>
+      <tr>
+        <td>Total of student</td>
+      </tr>
+      <tr>
+        <td>Total of student</td>
+      </tr>
+      <tr>
+        <td>Total of student</td>
+      </tr>
+      <tr><td></td></tr>
+    </table>
   </section>
 
   <!-- Footer -->
