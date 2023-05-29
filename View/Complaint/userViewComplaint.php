303e2a0ba660ec69abe686207955ebfefd2193ca
ainnajihah173 <ainnajihah173@gmail.com>
2023-05-29 23:41:09 +0800

    -

--- View/Complaint/userViewComplaint.php
+++ View/Complaint/userViewComplaint.php
@@ -60,7 +60,7 @@
                     <p>1</p>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-3 mt-3">
                 <canvas id="myChart" style="width:80%;max-width:300px"></canvas>
             </div>
         </div>
